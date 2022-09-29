31921c5689a2a0f5916ca0b4be38d163ea335aad
webdeveloper510 <developerweb6@gmail.com>
2022-09-29 11:56:09 +0530

    work well

--- MBV-REC/Record_Video.js
+++ MBV-REC/Record_Video.js
@@ -1,131 +1,268 @@
 /* eslint-disable */
-import React, { useState } from "react";
-import { Text, StyleSheet, View, Image, FlatList, TouchableOpacity,ScrollView } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+// import React, { useState } from "react";
+// import { Text, StyleSheet, View, Image, FlatList, TouchableOpacity,ScrollView } from "react-native";
+// import { useNavigation } from '@react-navigation/native';
 
-const data = [
-    { key: 'SEE' ,title:'wel 1videos'},
-    { key: 'VEE' ,title:'wel 2videos'},
-    { key: 'LEE' ,title:'wel 3videos'},
-   
-  
-  ]
-const Record_Video =()=>{
-  const navigation = useNavigation();
+// const data = [
+//     { key: 'SEE' ,title:'wel 1videos'},
+//     { key: 'VEE' ,title:'wel 2videos'},
+//     { key: 'LEE' ,title:'wel 3videos'},
 
-  const [arraydata, setarraydata] = useState(data);
 
-    return(
-      <ScrollView style={styles.container1}>
-<View style={styles.container}>
-      <View style={styles.TextsView}>
-        <Text style={styles.Text}>Mybest.video</Text>
-      </View>
-      <View style={styles.TableView}>
-        <View style={styles.RowView}>
-          <Text style={{color:'black',fontWeight: "bold"}}>RECORD VIDEO</Text>
-          
-        </View>
-      </View>
-     
+//   ]
+// const Record_Video =()=>{
+//   const navigation = useNavigation();
 
-      <View>
-        <FlatList
+//   const [arraydata, setarraydata] = useState(data);
 
-          data={arraydata}
+//     return(
+//       <ScrollView style={styles.container1}>
+// <View style={styles.container}>
+//       <View style={styles.TextsView}>
+//         <Text style={styles.Text}>Mybest.video</Text>
+//       </View>
+//       <View style={styles.TableView}>
+//         <View style={styles.RowView}>
+//           <Text style={{color:'black',fontWeight: "bold"}}>RECORD VIDEO</Text>
 
-          renderItem={({ item }) => 
-          <View  style={styles.ListedView}>
-          <View style={styles.GridedViewBlockStyle}>
-            <Text style={styles.GridedViewInsideTextItemStyle}> {item.key}</Text>
-          </View>
-        <View style={styles.GridedViewBlockStyle10}>
-            <Text style={styles.GridedViewInsideTextItemStyle}> {item.title}</Text>
+//         </View>
+//       </View>
 
-        </View>
-        </View>
-          }
 
-          numColumns={1}
+//       <View>
+//         <FlatList
 
-        />
-      </View>
+//           data={arraydata}
+
+//           renderItem={({ item }) => 
+//           <View  style={styles.ListedView}>
+//           <View style={styles.GridedViewBlockStyle}>
+//             <Text style={styles.GridedViewInsideTextItemStyle}> {item.key}</Text>
+//           </View>
+//         <View style={styles.GridedViewBlockStyle10}>
+//             <Text style={styles.GridedViewInsideTextItemStyle}> {item.title}</Text>
+
+//         </View>
+//         </View>
+//           }
+
+//           numColumns={1}
+
+//         />
+//       </View>
+//     </View>
+
+//     </ScrollView>
+
+
+
+
+
+//     )
+// }
+// const styles = StyleSheet.create({
+//          container: {
+//             flex: 1,
+//            backgroundColor: "#1c3567",
+//                 },
+//                 container1: {
+//                   flex: 1,
+//                   backgroundColor: "#1c3567"
+//                 },
+//          ListedView: {
+//            display:"flex",
+//            flexDirection:'row',
+//             width: "95%",
+//             alignSelf: 'center',
+//             marginTop:20
+//                    },
+
+//          RowView: {
+//              width: '60%',
+//              alignSelf: 'center',
+//              alignItems: 'center'
+//                    },
+//          TextsView: {
+//              alignSelf: 'center',
+//              width: "90%",
+//              alignItems: 'center',
+//              marginTop: 50,},
+//          Text: {
+//             fontFamily: "Cochin",
+//             color: 'white',},
+//          TableView: {
+//                 width: '95%',
+//                 height: 50,
+//                 backgroundColor: '#fbc112',
+//                 marginTop: 20,
+
+//                 marginLeft:10,
+//                 alignItems: 'center',
+//                 justifyContent: 'center'
+//               },
+//          GridedViewBlockStyle: {
+
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                     height: 90,
+//                     width:'30%',
+//                     margin: 5,
+//                     backgroundColor: '#999999'
+
+//                   },
+//          GridedViewBlockStyle10: {
+
+//                     width:'55%',
+
+//                   },
+//          GridedViewInsideTextItemStyle: {
+
+//                     color: '#fff',
+//                     padding: 10,
+//                     fontSize: 18,
+//                     justifyContent: 'center',
+
+//                   },
+//           button: {
+//                     alignItems: "center",
+//                     backgroundColor: "#DDDDDD",
+//                     padding: 10}
+// })
+// export default Record_Video;
+import React, { useState, useRef } from 'react';
+
+// import all the components we are going to use
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+//Import React Native Video to play video
+import Video from 'react-native-video';
+
+//Media Controls to control Play/Pause/Seek and full screen
+import
+MediaControls, { PLAYER_STATES }
+  from 'react-native-media-controls';
+
+const App = () => {
+  const videoPlayer = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [paused, setPaused] = useState(false);
+  const [
+    playerState, setPlayerState
+  ] = useState(PLAYER_STATES.PLAYING);
+  const [screenType, setScreenType] = useState('content');
+
+  const onSeek = (seek) => {
+    //Handler for change in seekbar
+    videoPlayer.current.seek(seek);
+  };
+
+  const onPaused = (playerState) => {
+    //Handler for Video Pause
+    setPaused(!paused);
+    setPlayerState(playerState);
+  };
+
+  const onReplay = () => {
+    //Handler for Replay
+    setPlayerState(PLAYER_STATES.PLAYING);
+    videoPlayer.current.seek(0);
+  };
+
+  const onProgress = (data) => {
+    // Video Player will progress continue even if it ends
+    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+      setCurrentTime(data.currentTime);
+    }
+  };
+
+  const onLoad = (data) => {
+    setDuration(data.duration);
+    setIsLoading(false);
+  };
+
+  const onLoadStart = (data) => setIsLoading(true);
+
+  const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
+
+  const onError = () => alert('Oh! ', error);
+
+  const exitFullScreen = () => {
+    alert('Exit full screen');
+  };
+
+  const enterFullScreen = () => { };
+
+  const onFullScreen = () => {
+    setIsFullScreen(isFullScreen);
+    if (screenType == 'content') setScreenType('cover');
+    else setScreenType('content');
+  };
+
+  const renderToolbar = () => (
+    <View>
+      <Text style={styles.toolbar}> toolbar </Text>
     </View>
+  );
 
-    </ScrollView>
+  const onSeeking = (currentTime) => setCurrentTime(currentTime);
 
+  return (
+    <View style={{ flex: 1 }}>
+      <Video
+        onEnd={onEnd}
+        onLoad={onLoad}
+        onLoadStart={onLoadStart}
+        onProgress={onProgress}
+        paused={paused}
+        ref={videoPlayer}
+        resizeMode={screenType}
+        onFullScreen={isFullScreen}
+        source={{
+          uri: RNFS.DocumentDirectoryPath + '/' + name
 
+        }}
+        style={styles.mediaPlayer}
+        volume={10}
+      />
+      <MediaControls
+        duration={duration}
+        isLoading={isLoading}
+        mainColor="#333"
+        onFullScreen={onFullScreen}
+        onPaused={onPaused}
+        onReplay={onReplay}
+        onSeek={onSeek}
+        onSeeking={onSeeking}
+        playerState={playerState}
+        progress={currentTime}
+        toolbar={renderToolbar()}
+      />
+    </View>
+  );
+};
 
+export default App;
 
-
-    )
-}
 const styles = StyleSheet.create({
-         container: {
-            flex: 1,
-           backgroundColor: "#1c3567",
-                },
-                container1: {
-                  flex: 1,
-                  backgroundColor: "#1c3567"
-                },
-         ListedView: {
-           display:"flex",
-           flexDirection:'row',
-            width: "95%",
-            alignSelf: 'center',
-            marginTop:20
-                   },
-
-         RowView: {
-             width: '60%',
-             alignSelf: 'center',
-             alignItems: 'center'
-                   },
-         TextsView: {
-             alignSelf: 'center',
-             width: "90%",
-             alignItems: 'center',
-             marginTop: 50,},
-         Text: {
-            fontFamily: "Cochin",
-            color: 'white',},
-         TableView: {
-                width: '95%',
-                height: 50,
-                backgroundColor: '#fbc112',
-                marginTop: 20,
-                
-                marginLeft:10,
-                alignItems: 'center',
-                justifyContent: 'center'
-              },
-         GridedViewBlockStyle: {
-               
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 90,
-                    width:'30%',
-                    margin: 5,
-                    backgroundColor: '#999999'
-                
-                  },
-         GridedViewBlockStyle10: {
-                    
-                    width:'55%',
-                   
-                  },
-         GridedViewInsideTextItemStyle: {
-                
-                    color: '#fff',
-                    padding: 10,
-                    fontSize: 18,
-                    justifyContent: 'center',
-                
-                  },
-          button: {
-                    alignItems: "center",
-                    backgroundColor: "#DDDDDD",
-                    padding: 10}
-})
-export default Record_Video;
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  mediaPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+});
